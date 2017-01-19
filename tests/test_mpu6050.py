8c31e6588a32561ed8d837346eb8a07072cc98b2
Jan Frederick Eick <jan-frederick.eick@uni-weimar.de>
2017-01-19 02:19:53 +0100

    added basic tests for mockup experiments

--- tests/test_mpu6050.py
+++ tests/test_mpu6050.py
@@ -4,20 +4,27 @@ import smbus
 from mock import MagicMock
 import pytest
 
+# NEW STYLE FIXTURE!!!
 @pytest.fixture
-def smbus_mock(mocker):
-    return MagicMock(smbus.SMBus)
-    #return mocker.patch.object(smbus, 'smbus.SMBus', autospech=True)
+def mpu6050_bus(mocker):
+    mock_smbus = mocker.patch.object(smbus, 'SMBus', autospec=True)
+    mpu = MPU6050(0x68)
+    return mpu
 
-@pytest.fixture
-def mpu6050_test_sensor(monkeypatch, smbus_mock):
-    monkeypatch.setattr('smbus.SMBus', smbus_mock)
-    test_sensor = MPU6050(0x68)
-    return test_sensor
+def test__MPU6050_init2(mpu6050_bus, mocker):
+    bus = mpu6050_bus.bus
+    #bus.read_byte_data = mocker.Mock()
+    bus.read_byte_data.side_effect = [1, 1]
+    i = mpu6050_bus.read_word(23)
+    assert i == 0x0101
+    bus.read_byte_data.assert_any_call(0x68, 23)
+    bus.read_byte_data.assert_any_call(0x68, 24)
 
-def test_createSensor(mpu6050_test_sensor):
-    assert mpu6050_test_sensor.address == 0x68
 
-def test_sensorbuilder_MPU6050(mpu6050_test_sensor):
-    sb_built_sensor = sb.createSensor(type="mpu6050", address="0x68")
-    #assert mpu6050_test_sensor.address == sb_built_sensor.address
+def test__MPU6050_init(mocker):
+    mock_smbus = mocker.patch.object(smbus, 'SMBus', autospec=True)
+    #bus = mocker.Mock()
+    #mock_smbus.return_value = bus
+    mpu = MPU6050(0x68)
+    #print bus.write_byte_data.call_args_list
+
