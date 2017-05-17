6a6723ea1fe5b1a5e21fd1dc1f63a38788596ca8
Jan Frederick Eick <jan-frederick.eick@uni-weimar.de>
2017-05-17 16:53:37 +0200

    updated versioneer

--- raspyre/__init__.py
+++ raspyre/__init__.py
@@ -1,7 +1,6 @@
 from pkgutil import extend_path
-from ._version import get_versions
-
 __path__ = extend_path(__path__, __name__)
 
+from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
